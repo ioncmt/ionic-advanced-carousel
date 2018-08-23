228846a0d1079da0e808b1abca9d980a859c81d4
krysalead <krysalead@gmail.com>
2018-08-23 08:25:37 +0200

    change build default and generate a new version

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,41 @@
-
-var gulp   = require('gulp');
+var gulp = require('gulp');
 var gutil = require('gulp-util');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglifyjs');
-var sass   = require('gulp-sass');
+var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var rename = require("gulp-rename");
 // var jshint = require('gulp-jshint');
 
-gulp.task('watch', function() {
-    gulp.watch(['src/**/*.js'], ['compile-js']);
-    gulp.watch('src/**/*.scss', ['build-styles']);
-    //gulp.watch(['src/custom-directives/**/*.*'], ['copy-items']);
+gulp.task('watch', function () {
+  gulp.watch(['src/**/*.js'], ['compile-js']);
+  gulp.watch('src/**/*.scss', ['build-styles']);
+  //gulp.watch(['src/custom-directives/**/*.*'], ['copy-items']);
 });
 
-gulp.task('compile-js', function() {
-    return gulp.src(['src/**/*.js'])
-            .pipe(sourcemaps.init())
-            .pipe(concat('advanced-carousel.min.js'))
-            .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())  // gulp --type production
-            .pipe(sourcemaps.write()) // Add the map to modified source.
-            .pipe(gulp.dest('dist/'));
+gulp.task('compile-js', function () {
+  return gulp.src(['src/**/*.js'])
+    .pipe(sourcemaps.init())
+    .pipe(concat('advanced-carousel.min.js'))
+    .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop()) // gulp --type production
+    .pipe(sourcemaps.write()) // Add the map to modified source.
+    .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('build-styles', function() {
-    var outputStyle = gutil.env.type === 'production' ? 'compressed' : 'nested';
-    return gulp.src(['src/**/*.scss'], {base:'src/'})
-        .pipe(sourcemaps.init())  // Process the original sources
-        .pipe(sass({outputStyle: outputStyle}))
-        .pipe(sourcemaps.write()) // Add the map to modified source.
-        .pipe(rename({suffix: ".min"}))
-        .pipe(gulp.dest('dist/'));
+gulp.task('build-styles', function () {
+  var outputStyle = gutil.env.type === 'production' ? 'compressed' : 'nested';
+  return gulp.src(['src/**/*.scss'], {
+      base: 'src/'
+    })
+    .pipe(sourcemaps.init()) // Process the original sources
+    .pipe(sass({
+      outputStyle: outputStyle
+    }))
+    .pipe(sourcemaps.write()) // Add the map to modified source.
+    .pipe(rename({
+      suffix: ".min"
+    }))
+    .pipe(gulp.dest('dist/'));
 });
 
 //gulp.task('copy-items', function() {
@@ -38,5 +43,4 @@ gulp.task('build-styles', function() {
 //        .pipe(gulp.dest('dist/'))
 //});
 
-gulp.task('default', ['watch']);
-
+gulp.task('default', ['compile-js', 'build-styles']);
